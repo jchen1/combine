14ceb14639e3073a03179af4b71de165f5760dd6
Jeff Chen <jchen@ladderlife.com>
2021-06-03 12:39:35 -0400

    title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,25 @@
 import type { AppProps /*, AppContext */ } from "next/app";
 
 import { GeistProvider, CssBaseline } from "@geist-ui/react";
+import { useRouter } from "next/router";
 
 import Meta from "../components/Meta";
 
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { asPath } = useRouter();
+
   const metas = {
     "twitter:creator": "@iambald",
     "twitter:site": "@iambald",
-    "twitter:card": "summary",
-    // todo parameterize
-    "og:url": `https://nflcombinestats.com`,
+    "twitter:card": "summary_large_image",
+    // todo parameterize BASE_URL
+    "og:url": `https://nflcombinestats.com${asPath}`,
     "og:image": `https://nflcombinestats.com/images/og.png`,
     description: "Test how you stack up compared to NFL players!",
     "og:type": "website",
+    "og:title": "NFL Combine Comparator",
   };
 
   return (
