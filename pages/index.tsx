2622f2f6eed526ada17ee6a63e3903eba5a27781
Jeff Chen <jchen@ladderlife.com>
2021-06-02 20:12:10 -0400

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,6 @@ import {
   Spacer,
   Text,
   Toggle,
-  useMediaQuery,
 } from "@geist-ui/react";
 import { Radar } from "react-chartjs-2";
 import { useEffect, useState } from "react";
@@ -49,9 +48,11 @@ function copyShareLink(player: CombineResult): Promise<void> {
   return navigator.clipboard.writeText(getShareLink(player));
 }
 
-const IndexPage = ({ query }: { query: Record<string, string | string[]> }) => {
-  const isDesktop = useMediaQuery("md", { match: "up" });
+function isNilOrDefault(x: any, d: any) {
+  return x === null || x === undefined ? d : x;
+}
 
+const IndexPage = ({ query }: { query: Record<string, string | string[]> }) => {
   const [copied, setCopied] = useState(false);
 
   const [comparePlayers, setComparePlayers] = useState(true);
@@ -61,28 +62,28 @@ const IndexPage = ({ query }: { query: Record<string, string | string[]> }) => {
   );
   const [name, setName] = useState(stringOrFirst(query.player) || "My Player");
   const [height, setHeight] = useState(
-    parseInt(stringOrFirst(query.height)) || 73
+    isNilOrDefault(parseInt(stringOrFirst(query.height)), 73)
   );
   const [weight, setWeight] = useState(
-    parseInt(stringOrFirst(query.weight)) || 184
+    isNilOrDefault(parseInt(stringOrFirst(query.weight)), 184)
   );
   const [fortyYard, setFortyYard] = useState(
-    parseFloat(stringOrFirst(query.fortyYard)) || 4.29
+    isNilOrDefault(parseFloat(stringOrFirst(query.fortyYard)), 4.29)
   );
   const [verticalJump, setVerticalJump] = useState(
-    parseFloat(stringOrFirst(query.verticalJump)) || 38.5
+    isNilOrDefault(parseFloat(stringOrFirst(query.verticalJump)), 38.5)
   );
   const [benchReps, setBenchReps] = useState(
-    parseInt(stringOrFirst(query.benchReps)) || 17
+    isNilOrDefault(parseInt(stringOrFirst(query.benchReps)), 17)
   );
   const [broadJump, setBroadJump] = useState(
-    parseInt(stringOrFirst(query.broadJump)) || 131
+    isNilOrDefault(parseInt(stringOrFirst(query.broadJump)), 131)
   );
   const [threeCone, setThreeCone] = useState(
-    parseFloat(stringOrFirst(query.threeCone)) || 6.74
+    isNilOrDefault(parseFloat(stringOrFirst(query.threeCone)), 6.74)
   );
   const [shuttleRun, setShuttleRun] = useState(
-    parseFloat(stringOrFirst(query.shuttleRun)) || 4.17
+    isNilOrDefault(parseFloat(stringOrFirst(query.shuttleRun)), 4.17)
   );
 
   const player: CombineResult = {
@@ -208,7 +209,7 @@ const IndexPage = ({ query }: { query: Record<string, string | string[]> }) => {
       <Grid.Container gap={2} justify="center" wrap="wrap" id="main">
         <Grid xs={24} md={16} id="chart">
           <Card>
-            <Row align="middle" justify={isDesktop ? "end" : "center"}>
+            <Row align="middle" justify="center">
               <Toggle
                 checked={comparePlayers}
                 size="large"
@@ -217,7 +218,6 @@ const IndexPage = ({ query }: { query: Record<string, string | string[]> }) => {
               <Spacer x={0.5} />
               <Text>Show comparable players</Text>
             </Row>
-            {isDesktop && <Spacer y={1} />}
             <Radar type="radar" data={data} options={options} />
           </Card>
         </Grid>
@@ -356,7 +356,7 @@ const IndexPage = ({ query }: { query: Record<string, string | string[]> }) => {
         <Grid xs={24} md={24} id="share">
           <Row style={{ width: "100%" }}>
             <Button
-              style={!isDesktop ? { width: "100%" } : {}}
+              style={{ width: "100%" }}
               type="success"
               onClick={async (_e) => {
                 await copyShareLink(player);
